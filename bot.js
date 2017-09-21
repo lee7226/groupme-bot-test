916d3fa7a2cdfecbe00d9bbd2af9180a5f5fd10c
lee7226 <bxlee1441@gmail.com>
2017-09-21 18:31:17 -0400

    test

--- bot.js
+++ bot.js
@@ -3,6 +3,10 @@ var cool = require('cool-ascii-faces');
 
 var botID = process.env.BOT_ID;
 
+
+//TODO: Get the response to trigger correctly whenever the trigger appears in a message
+
+
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegex = /^\/Bill$/,
@@ -11,7 +15,44 @@ function respond() {
 	var str = request.text.toString();
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);	
-    postMessage();
+    //postMessage();
+	
+	/////////////////
+	var botResponse, options, body, botReq;
+  var str = "does this work?" + " how about this? " + request.text.toString() + " end";
+
+  botResponse = str;//this.req.toString();//cool();
+
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+	/////////////////
+	
     this.res.end();
   } else {
     console.log("don't care");
